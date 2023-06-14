695374f7b876435af91b364f7c62570971d51129
SethPython <spccmbseth@gmail.com>
2023-06-14 21:19:15 +0530

    Update Executable

--- SpritzTerminal/SpritzTerminal/SpritzTerminal.cpp
+++ SpritzTerminal/SpritzTerminal/SpritzTerminal.cpp
@@ -6,8 +6,17 @@ int main(int argc, char *argv[])
 {
     bool foundJRE = false;
 
-    string path = std::getenv("PATH");
-    
+    string path = "";
+    char* buf = nullptr;
+    size_t sz = 0;
+    if (_dupenv_s(&buf, &sz, "PATH") == 0 && buf != nullptr)
+    {
+        path = buf;
+        free(buf);
+    }
+    if (path.find("java") != std::string::npos) {
+        foundJRE = true;
+    }
 
 
     if (!foundJRE) {
